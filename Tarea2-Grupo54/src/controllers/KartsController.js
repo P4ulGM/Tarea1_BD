c3baad7ad033236a9c5d50c9c79fe74cac8a3b38
Paolo García <74685658+P4ulGM@users.noreply.github.com>
2023-06-10 21:34:11 -0400

    kartsController
    CREATE / UPDATE falta codigo para comprobar existencia del id en PERSONAJES

--- Tarea2-Grupo54/src/controllers/KartsController.js
+++ Tarea2-Grupo54/src/controllers/KartsController.js
@@ -1,16 +1,41 @@
 import prisma from '../prismaClient.js'
 
-const createKarts = async (req, res) => {
+const createKart = async (req, res) => {
     const { modelo, color, velocidad_maxima, id_personaje } = req.body
-    const kart = await prisma.karts.create({
-        data: {
-            modelo,
-            color,
-            velocidad_maxima,
-            id_personaje
-        }
-    })
-    res.json(kart)
+    
+    if (!modelo || !color || !velocidad_maxima || !id_personaje){
+        return res.status(400).json({mensaje: "Modelo, Color, Velocidad maxima y Id_personaje son obligatorias"})
+    }
+
+    if (
+        typeof modelo !== "string" ||
+        typeof color !== "string" ||
+        typeof velocidad_maxima !== "number" ||
+        typeof id_personaje  !== "number") {
+        return res.status(400).json({mensaje: "Error en tipo de Modelo, Color, Velocidad maxima o Id_personaje"})
+    }
+
+    if(modelo.length > 45) {
+        return res.status(400).json({mensaje: " Nombre del Modelo muy largo"})
+    }
+
+    if(color.length > 45) {
+        return res.status(400).json({mensaje: " Nombre del Color muy largo"})
+    }
+
+    try {
+        const kart = await prisma.karts.create({
+            data: {
+                modelo,
+                color,
+                velocidad_maxima,
+                id_personaje
+            }
+        })
+        res.json(kart)
+    } catch {
+        res.status(500).json({mensaje: "Error al crear el kart"})
+    }
 }
 
 const getKarts = async (req , res) => {
@@ -20,42 +45,64 @@ const getKarts = async (req , res) => {
 
 const getKartById = async (req, res) => {
     const { id } = req.params
-    const kart = await prisma.karts.findUnique({
-        where: {
-            id: Number(id)
-        }
-    })
-    if (!kart)
-        return res.status(404).json({error: "Kart not found"});
-    res.json(kart)
+    try{
+        const kart = await prisma.karts.findUniqueOrThrow({
+            where: {
+                id: Number(id)
+            }
+        })
+        res.json(kart)  
+     } catch { 
+        res.status(404).json({mensaje: "Kart no encontrado"});
+     }
 }
 
 const updateKart = async (req, res) => {
-    const { id } = req.params
-    const updatekart = await prisma.karts.update({
-        where: {
-            id: Number(id)
-        },
-        data: req.body
-    })
-    res.json(updatekart)
+    let { id } = req.params
+    const { modelo, color, velocidad_maxima } = req.body
+    if(!id) {
+        return res.status(400).json({mensaje: "Se necesita un id para actualizar"})
+    } else try {
+        id = Number(id)
+    } catch {
+        return res.status(400).json({mensaje: "id debe de ser un número"})
+    }
+    const DatosActalizados = {
+        modelo, 
+        color, 
+        velocidad_maxima
+    }
+
+    try {
+        const kart = await prisma.karts.update({
+            where: {id: id},
+            data: DatosActalizados
+        });
+        res.status(200).json(kart);
+    } catch {
+        res.status(500).json({mensaje: "Error al crear kart"})
+    }
 }
 
 const deleteKart = async (req, res) => {
     const { id } = req.params
-    const deletekart = await prisma.karts.delete({
-        where: {
-            id: Number(id)
-        }
-    })
-    if (!deletekart)
-        return res.status(404).json({error: "Kart not found"});
-    res.json(deletekart)
+    try {
+        const deletekart = await prisma.karts.delete({
+            where: {
+                id: Number(id)
+            }
+        })
+        res.json(deletekart)
+    } catch {
+        res.status(500).json({mensaje: "Error al borrar kart"})
+    }
+
+    
 }
 
 
 const KartsController = {
-    createKarts,
+    createKart,
     getKarts,
     getKartById,
     updateKart,
