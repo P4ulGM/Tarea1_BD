a264ae09874a3822e0ae982c00381dfcee7ebecf
Pablo Guzmán <pablo.guzmanca@sansano.usm.cl>
2023-06-11 22:16:00 -0400

    Ultimos ajustes CRUD

--- Tarea2-Grupo54/src/controllers/Personaje_habita_reinoController.js
+++ Tarea2-Grupo54/src/controllers/Personaje_habita_reinoController.js
@@ -1,33 +1,25 @@
 import prisma from '../prismaClient.js'
 
 const createPersonaje_habita_reino = async (req, res) => {
-    const { id_personaje, id_reino, fecha_registro, es_gobernante } = req.body
+    const { id_personaje, id_reino, es_gobernante } = req.body
 
-    if (!id_personaje || !id_reino || !fecha_registro || !es_gobernante) {
-        return res.status(400).json({mensaje: "id_personaje, id_reino, fecha_registro, es_gobernante son obligatorias"})
+    if (!id_personaje || !id_reino) {
+        return res.status(400).json({mensaje: "id_personaje, id_reino, es_gobernante son obligatorias"})
     }
 
    if (
        typeof id_personaje !== "number" ||
        typeof id_reino !== "number" ||
-       typeof fecha_registro  !== "string" ||
        typeof es_gobernante !== "boolean") {
-        return res.status(400).json({mensaje: "Error en tipo de id_personaje, id_reino, fecha_registro, es_gobernante"})
+        return res.status(400).json({mensaje: "Error en tipo de id_personaje, id_reino, es_gobernante"})
     }
 
-    let fechaRegistroObj = Date.parse(fecha_registro)
-    if(isNaN(fechaRegistroObj)) {
-        return res.status(400).json({mensaje: "Fecha con formato incorrectos"})
-    } else {
-        fechaRegistroObj = new Date(fechaRegistroObj)
-    }
 
     try {
         const Personaje_habita_reino = await prisma.personaje_habita_reino.create({
             data: {
                 id_personaje,
-                id_reino,
-                fecha_registro, 
+                id_reino, 
                 es_gobernante
             }
         })
@@ -39,57 +31,62 @@ const createPersonaje_habita_reino = async (req, res) => {
 
 const getPersonaje_habita_reino = async (req , res) => {
     const Personaje_habita_reino = await prisma.personaje_habita_reino.findMany()
-    res.json(Personaje_habita_reino)
+    res.status(200).json(Personaje_habita_reino)
 }
 
 const getPersonaje_habita_reinoById = async (req, res) => {
-    const { id_personaje } = req.params
+    const { id } = req.params
+    let Personaje_habita_reino
     try{
-        const Personaje_habita_reino = await prisma.personaje_habita_reino.findMany({
-            where: {
-                id_personaje: Number(id_personaje)
-            }
-        })
+        if(req.query.tipo == "personaje") {
+            Personaje_habita_reino = await prisma.personaje_habita_reino.findMany({
+                where: {
+                    id_personaje: Number(id)
+                }
+            })
+        } else if (req.query.tipo == "reino") {
+            Personaje_habita_reino = await prisma.personaje_habita_reino.findMany({
+                where: {
+                    id_reino: Number(id)
+                }
+            })
+        } else {
+            return res.status(400).json({mensaje: "Error en parametro tipo"})
+        }
         res.json(Personaje_habita_reino) 
     } catch {
-        res.status(404).json({mensaje: 'personaje no habita ningun reino'})
+        res.status(500).json({mensaje: 'Error al buscar habitante'})
     }
 }
 
 const updatePersonaje_habita_reino = async (req, res) => {
     let { id_personaje, id_reino} = req.params
-    const { fecha_registro, es_gobernante } = req.body
+    const { es_gobernante } = req.body
 
     if(!id_personaje || !id_reino) {
         return res.status(400).json({mensaje: "Se necesita un id_personaje e id_reino para actualizar"})
-    } else try {
-        id_personaje = Number(id_personaje);
-        id_reino = Number(id_reino)
-    } catch {
-        return res.status(400).json({mensaje: "id_personaje o id_reino debe de ser un número "})
     }
     
-    let fecha_registro_formateada;
-    if (fecha_registro){
-        fecha_registro_formateada = Date.parse(fecha_registro)
-        if (isNaN(fecha_registro_formateada)) {
-            return res.status(400).json({mensaje: "Fecha con formato incorrectos"}) 
-        }
-        fecha_registro_formateada = new Date(fecha_registro_formateada)
+
+    id_personaje = Number(id_personaje)
+    id_reino = Number(id_reino)
+
+    if(isNaN(id_personaje) || isNaN(id_reino)) {
+        return res.status(400).json({mensaje: "id_personaje y id_reino debe de ser un número "})
     }
 
-    const DatosActualizados = {
-        fecha_registro:fecha_registro_formateada, 
-        es_gobernante
-    }
 
     try {
-        const updatepersonaje_habita_reino = await prisma.personaje_habita_reino.updateMany({
-            where: {
-                id_personaje: parseInt(req.params.id_personaje),
-                id_reino: parseInt(req.params.id_reino)},
-                data: DatosActualizados
-        });
+        const updatepersonaje_habita_reino = await prisma.personaje_habita_reino.update({
+            where: {id_personaje_id_reino: {
+                id_personaje,
+                id_reino
+            }},
+            data: {
+                es_gobernante
+            }
+        }
+        );
         res.status(200).json(updatepersonaje_habita_reino);
     } catch {
         res.status(500).json({mensaje: "Error al actualizar personaje en el reino"})
@@ -100,17 +97,11 @@ const updatePersonaje_habita_reino = async (req, res) => {
 const deletePersonaje_habita_reino = async (req, res) => {
     const { id_personaje, id_reino } = req.params
     try {
-        const deletepersonaje_habita_reino = await prisma.personaje_habita_reino.deleteMany({
-            where: {
-                AND: [
-                  {
-                      id_personaje: Number(id_personaje),
-                  },
-                  {
-                      id_reino: Number(id_reino)
-                  }
-                ]
-              }
+        const deletepersonaje_habita_reino = await prisma.personaje_habita_reino.delete({
+            where: {id_personaje_id_reino: {
+                id_personaje: Number(id_personaje),
+                id_reino: Number(id_reino)
+            }}
         })
         res.status(200).json(deletepersonaje_habita_reino)
     } catch  {
